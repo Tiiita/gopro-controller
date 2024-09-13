942362229c379b3ad7f300787786c649468eaecf
earomc <moritz.brunne@gmail.com>
2024-09-13 22:21:42 +0200

    more uses for CommandResult type alias

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,9 @@
 use colored::Colorize;
 use futures::executor;
 use gopro_controller as gp_ctrl;
-use crate::{command::{CommandError, Context}, controller::{self, GoPro}};
+use crate::{command::{CommandError, CommandResult, Context}, controller::{self, GoPro}};
 
-pub fn help_cmd(context: Context) -> Result<(), CommandError> {
+pub fn help_cmd(context: Context) -> CommandResult {
     let commands = &context.cmd_service.commands;
 
     let max_usage_len = commands
@@ -27,7 +27,7 @@ pub fn help_cmd(context: Context) -> Result<(), CommandError> {
     Ok(())
 }
 
-pub fn device_cmd<'a>(context: Context<'a>) -> Result<(), CommandError<'a>> {
+pub fn device_cmd<'a>(context: Context<'a>) -> CommandResult<'a> {
     if context.args.is_empty() {
         return Err(CommandError::Syntax);
     }
@@ -96,6 +96,6 @@ pub fn device_cmd<'a>(context: Context<'a>) -> Result<(), CommandError<'a>> {
     Ok(())
 }
 
-pub fn record_cmd(_context: Context) -> Result<(), CommandError> {
+pub fn record_cmd(_context: Context) -> CommandResult {
     Ok(())
 }
